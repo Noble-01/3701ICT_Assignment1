974ba407585a157295694d54ee5701d708bdbdcd
Noble-01 <zaccripps5@gmail.com>
2020-03-25 21:46:54 -0700

    reorganising of views (split masterview between contentview

--- Milestone1/View/ContentView.swift
+++ Milestone1/View/ContentView.swift
@@ -10,10 +10,22 @@ import SwiftUI
 
 struct ContentView : View {
     ///is viewd by the viewmodel for any changes done to the array
-    @ObservedObject var viewModel:ViewModel
+    @ObservedObject 	var viewModel:ViewModel
     
     ///shows the MasterView with then utalizes the ViewModel file
     var body: some View{
-        MasterView(viewModel: viewModel)
+                NavigationView {
+                    MasterView(viewModel: viewModel)
+                    .navigationBarTitle(Text(self.viewModel.listTitle))
+                    .navigationBarItems(
+                        leading: EditButton(),
+                        trailing:HStack {
+                            Button(action:{withAnimation{self.viewModel.addElement()}
+                            }){
+                                Image(systemName: "plus")
+                            }
+                        }
+                    )
+        }
     }
 }
--- Milestone1/View/MasterView.swift
+++ Milestone1/View/MasterView.swift
@@ -16,45 +16,32 @@ struct MasterView: View {
     @ObservedObject var viewModel: ViewModel
     
     var body: some View {
-        NavigationView{
-            MasterView(viewModel: viewModel)
-       ///creates the list that utalises the arrau of potato information
-                List(viewModel.potatos){potato in
-                ///creates a for loop to run through the array with the var potato
-                    NavigationLink(destination: DetailView(model: potato)){
-                        Section{
-                            ///places the elements in a single line
-                            HStack(){
-                            Image(potato.image).resizable()
-                            .padding(.vertical, 5.0)
-                            .frame(width: 80.0, height: 80.0)
-                            .shadow(radius: 10)
-                            .scaledToFit()
-                                
-                            Text(potato.name).fontWeight(.bold)
-                            Text(potato.scienceName)
-                            .font(.subheadline)
-                            .fontWeight(.light)
-                            .italic()
-                            .frame(maxWidth: .infinity, alignment: .trailing)
-                                }
+        ///creates the list that utalises the array of potato information
+        List{
+            ///creates a for loop to run through the array with the var potato
+            ForEach(viewModel.potatos) { potato in
+                NavigationLink(destination: DetailView(model: potato)){
+                        ///places the elements in a single line
+                        HStack(){
+                        Image(potato.image).resizable()
+                        .padding(.vertical, 5.0)
+                        .frame(width: 80.0, height: 80.0)
+                        .shadow(radius: 10)
+                        .scaledToFit()
+                            
+                        Text(potato.name).fontWeight(.bold)
+                        Text(potato.scienceName)
+                        .font(.subheadline)
+                        .fontWeight(.light)
+                        .italic()
+                        .frame(maxWidth: .infinity, alignment: .trailing)
                             }
-                    }///Title of navigation
-                    .navigationBarTitle(Text(self.viewModel.listTitle))
-                    .navigationBarItems(
-                        leading: EditButton(),
-                        trailing:HStack {
-                            Button(action:{withAnimation{self.viewModel.addElement()}
-                            }){
-                                Image(systemName: "plus")
-                            }
-                        }
-                    )
                 }
-            .onDelete(perform: deleteItems)
-            }
-        }
+        }.onDelete(perform: deleteItems)
+}
+
+}
     func deleteItems(at offsets: IndexSet){
-        viewModel.potatos.remove(atOffsets: offsets)
+    viewModel.potatos.remove(atOffsets: offsets)
     }
 }
